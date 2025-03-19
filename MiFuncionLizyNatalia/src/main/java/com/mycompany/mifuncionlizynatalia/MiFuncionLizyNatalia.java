f59a9f65ddcb844530dbd39e99a25d2dcba8fe34
Natalia <jessicanatalia.cortes.b2005@gmail.com>
2025-03-19 08:32:17 -0500

    Add files via upload

---
+++ MiFuncionLizyNatalia/src/main/java/com/mycompany/mifuncionlizynatalia/MiFuncionLizyNatalia.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mifuncionlizynatalia;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class MiFuncionLizyNatalia {
+
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(84,99);
+                System.out.println(resultado);
+                }
+    public static int suma(int a,int b){
+    return a + b;
+    
+}
+}
