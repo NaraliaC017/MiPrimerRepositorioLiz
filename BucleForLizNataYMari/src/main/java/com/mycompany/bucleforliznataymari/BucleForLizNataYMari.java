37ce238a918fac6de851bea5656aa671e4d22f89
Natalia <jessicanatalia.cortes.b2005@gmail.com>
2025-04-02 08:50:06 -0500

    Add files via upload

---
+++ BucleForLizNataYMari/src/main/java/com/mycompany/bucleforliznataymari/BucleForLizNataYMari.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.bucleforliznataymari;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class BucleForLizNataYMari {
+
+    public static void main(String[] args) {
+        int valores[] = {10, 15, 20, 25, 30, 35, 40};
+        
+        for (int i=2; i <= valores.length; i++){
+        System.out.println(valores[i]); 
+        }
+    }
+}
