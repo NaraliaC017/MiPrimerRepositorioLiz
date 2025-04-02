5acef6dde0941d40db10333e0c1f15946b425255
Natalia <jessicanatalia.cortes.b2005@gmail.com>
2025-04-02 08:50:53 -0500

    Add files via upload

---
+++ CondicionalesLizNataYMariana/src/main/java/com/mycompany/condicionalesliznataymariana/CondicionalesLizNataYMariana.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.condicionalesliznataymariana;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class CondicionalesLizNataYMariana {
+
+    public static void main(String[] args) {
+        String meses = "Octubre";
+        
+        if (meses == "Octubre"){
+        System.out.println("Disfrazarse");
+        System.out.println("Comer Dulces");
+        System.out.println("Asustar");
+        }else if (meses == "Diciembre"){
+            System.out.println("Estrenar ropa");
+        System.out.println("Abrir regalos");
+        System.out.println("Comer buñuelos y natilla");
+    System.out.println("Cantar villancicos");
+    }else {
+      System.out.println("Es otro Mes...");
+        
+       }
+    }
+}
