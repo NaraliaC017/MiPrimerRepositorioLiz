d4fe96dd6c40fd70a013fca2dbf1a798916ea17b
Natalia <jessicanatalia.cortes.b2005@gmail.com>
2025-04-02 08:50:35 -0500

    Add files via upload

---
+++ BucleWhileNataMariYLiz/src/main/java/com/mycompany/buclewhilenatamariyliz/BucleWhileNataMariYLiz.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.buclewhilenatamariyliz;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class BucleWhileNataMariYLiz {
+
+    public static void main(String[] args) {
+        int contador = 45;
+        
+        while (contador>0){
+            System.out.println(contador);
+            
+            contador = contador -1;
+        }
+    }
+}
